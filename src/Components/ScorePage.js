9f96b012a47baf30cea80225019a09944a443bf4
Jordan Smithson <jordantroysmithson@gmail.com>
2020-06-15 16:18:53 -0600

    working on reack hooks

--- src/Components/ScorePage.js
+++ src/Components/ScorePage.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ScorePage = () => {
+
+  const [count, setCount] = useState(0)
+  const [testVal, setTestVal] = useState()
+
+  const showMe = (e) => {
+    setTestVal(e.target.value)
+    console.log(testVal)
+  }
+
   return (
     <div>
       <h1>Score Page</h1>
+      <h2>You have clicked {count} times</h2>
+      <button 
+        onClick={() => setCount(count + 1)}
+      >Click Me</button>
+
+      <input type="text" onChange={(e)=>showMe(e)}/>
     </div>
   )
 }
