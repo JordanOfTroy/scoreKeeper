7af91c7a0051dffd27a717c745f4aa5956d0d072
Jordan Smithson <jordantroysmithson@gmail.com>
2020-06-16 13:39:23 -0600

    added Bootstrap CSS CDN and FontAwesome kit. Both seem to be working.

--- src/Components/StartPage.js
+++ src/Components/StartPage.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 const StartPage = () => {
   return (
-    <div>
-      <h1>Start Page</h1>
+    <div className='row d-flex align-items-center justify-content-center'>
+      <div className="col-4 d-flex align-items-center justify-content-center">
+        <i class="fas fa-space-station-moon-alt fa-9x"></i>
+      </div>
+      <div className="col-4 d-flex align-items-center justify-content-center">
+        <i class="fas fa-space-station-moon-alt fa-9x"></i>
+      </div>
+      <div className="col-4 d-flex align-items-center justify-content-center">
+        <i class="fas fa-space-station-moon-alt fa-9x"></i>
+      </div>
     </div>
   )
 }
